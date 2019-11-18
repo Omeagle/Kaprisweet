b85a223033630755b0843ba123908363eeed20cb
Julio Medina <holkito@gmail.com>
2019-11-17 20:29:57 -0600

    Ediciones Varias

--- contactos.php
+++ contactos.php
@@ -1,7 +1,6 @@
 <?php 
 include("config/db.php");
 include("includes/header.php"); 
-include("includes/carousel.php"); 
 
 
 if($_SERVER['REQUEST_METHOD']=='POST'){
@@ -9,6 +8,46 @@ if($_SERVER['REQUEST_METHOD']=='POST'){
     print_r($_POST);
 }
 ?>
+
+<!-- Seccion Banner -->
+
+<section class="banner_contact">
+        <img src="img/bannercontact.jpg" class="img-fluid" alt="Banner">    
+</section>
+
+<!-- Seccion Separador -->
+<div class="separador2"></div>
+
+
+<section class="body_contact">
+    <div class="container">
+
+            <form>
+            <div class="form-group">
+                <label for="exampleFormControlInput1">Correo Electronico</label>
+                <input type="email" class="form-control" id="exampleFormControlInput1" placeholder="name@example.com">
+            </div>
+            <div class="form-group">
+                <label for="exampleFormControlInput1">Asunto</label>
+                <input type="email" class="form-control" id="exampleFormControlInput1" placeholder="name@example.com">
+            </div>     
+            <div class="form-group">
+                <label for="exampleFormControlTextarea1">Mensaje</label>
+                <textarea class="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>                
+            </div>
+            <button type="submit" class="btn btn-primary">Enviar</button>
+            </form>
+
+
+
+    </div>
+</section>
+
+
+
+
+
+
 <div class="container content">
 <form action="" method="post" name="contact">
     <input type="text" class="text" name="name">
